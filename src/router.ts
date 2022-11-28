06fa5fcff9f491a59ee156c051de3c41217dbecf
riolly <alberto.riolly@gmail.com>
2022-11-28 14:39:09 +0700

    :coffin: refactor: clean up boilerplate

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,6 @@
 import {createRouteConfig, createReactRouter} from '@tanstack/react-router'
 
 import HomePage from 'pages/home'
-import ExamplePage from 'pages/example'
 
 const rootRoute = createRouteConfig()
 
@@ -9,12 +8,8 @@ const indexRoute = rootRoute.createRoute({
 	path: '/',
 	component: HomePage,
 })
-const exampleRoute = rootRoute.createRoute({
-	path: '/example',
-	component: ExamplePage,
-})
 
-const routeConfig = rootRoute.addChildren([indexRoute, exampleRoute])
+const routeConfig = rootRoute.addChildren([indexRoute])
 const router = createReactRouter({routeConfig})
 
 export default router
