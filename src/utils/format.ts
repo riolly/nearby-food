2034600f01689050312b7ee56c7d3a6111df7abb
riolly <alberto.riolly@gmail.com>
2022-11-28 19:02:47 +0700

    :hammer: chore(utils): format distance

---
+++ src/utils/format.ts
@@ -0,0 +1,20 @@
+export const roundDistance = (distance: number) => {
+	let unit = ''
+	let rounded = 0
+	if (distance < 100) {
+		rounded = Math.round(distance / 10) * 10
+		unit = 'm'
+	} else if (distance < 1000) {
+		rounded = Math.round(distance / 100) * 100
+		unit = 'm'
+		if (rounded === 1000) {
+			rounded = 1
+			unit = 'km'
+		}
+	} else {
+		rounded = Math.round(distance / 1000)
+		unit = 'km'
+	}
+
+	return `~${rounded} ${unit}`
+}
