27ca95d97f32a5d4aaf77ea1606c1c46a3a972c4
riolly <alberto.riolly@gmail.com>
2022-11-28 19:08:51 +0700

    :sparkles: feat: show address & distance

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import NavbarTopLayout from 'layouts/navbar'
+
+import {MapPinIcon} from '@heroicons/react/24/solid'
+
 import {usePlaces} from 'utils/hooks'
+import {roundDistance} from 'utils/format'
+
+import {type Place} from 'types/places'
 
 function HomePage() {
 	const {data, isError, isLoading, error} = usePlaces()
 
 	return (
 		<NavbarTopLayout>
-			<main className='flex flex-col items-center gap-8'>
+			<main className='flex flex-col items-center gap-8 px-4'>
 				<h1>Food & Beverages Near You</h1>
 
-				<div className='grid grid-cols-3 gap-2'>
+				<div className='grid grid-cols-3 gap-3'>
 					{isLoading ? (
 						<p>Loading ...</p>
 					) : isError ? (
@@ -18,18 +24,35 @@ function HomePage() {
 					) : (Array.isArray(data) && data.length === 0) || data === null ? (
 						<p>Empty ...</p>
 					) : (
-						data.map((place) => (
-							<div
-								key={place.fsq_id}
-								className='col-span-full rounded-xl bg-primary-lightest/20 p-6'
-							>
-								<h2>{place.name}</h2>
-							</div>
-						))
+						data.map((place) => <Card key={place.fsq_id} {...place} />)
 					)}
 				</div>
 			</main>
 		</NavbarTopLayout>
+	)
+}
+
+function Card({fsq_id, name, location, distance}: Place) {
+	return (
+		<div
+			key={fsq_id}
+			className='col-span-full rounded-xl bg-primary-lightest/20 py-4 px-6'
+		>
+			<h2>{name}</h2>
+			<div className='flex justify-between'>
+				<div className='flex gap-2'>
+					<div className='mt-1 w-fit'>
+						<MapPinIcon className='h-5 w-5 text-secondary-normal' />
+					</div>
+					<p>{location.address}</p>
+				</div>
+				<div className='mt-1 w-fit'>
+					<p className='w-16 text-right text-base font-semibold italic'>
+						{roundDistance(distance)}
+					</p>
+				</div>
+			</div>
+		</div>
 	)
 }
 
