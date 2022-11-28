8dd5a8af50c31b5aa3632c740fbd42d16d508076
riolly <alberto.riolly@gmail.com>
2022-11-28 16:51:39 +0700

    :wrench: change: stale to 60s, no refetch on window focus

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,11 @@ import {RouterProvider, Outlet} from '@tanstack/react-router'
 import router from './router'
 
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {staleTime: 60_000, refetchOnWindowFocus: false},
+	},
+})
 
 import './index.css'
 
