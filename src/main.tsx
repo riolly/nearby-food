c660700dfa536fc6fef6715c354c709f9c00aad7
riolly <alberto.riolly@gmail.com>
2022-11-29 19:29:11 +0700

    :wrench: change: stale time 1 hour, cache time 1 day

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import router from './router'
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 const queryClient = new QueryClient({
 	defaultOptions: {
-		queries: {staleTime: 60_000, refetchOnWindowFocus: false},
+		queries: {staleTime: 1000 * 60 * 60, cacheTime: 1000 * 60 * 60 * 24},
 	},
 })
 
