8b718530afaf680e6e5583f670fe6b592053fb0f
riolly <alberto.riolly@gmail.com>
2022-12-05 12:19:33 +0700

    :hammer: chore: usePhotos & useTips

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -1,6 +1,11 @@
 import {useQuery, type UseQueryOptions} from '@tanstack/react-query'
 import {defaultCategoryId, defaultRadius} from 'utils/constant'
-import {type PlaceDetails, type PlaceList} from 'types/place'
+import {
+	type Photo,
+	type Tip,
+	type PlaceDetails,
+	type PlaceList,
+} from 'types/place'
 
 const reqOpts = {
 	method: 'GET',
@@ -77,10 +82,9 @@ const detailFieldsUri = encodeURIComponent(detailFields.join(','))
 export const usePlaceDetails = (
 	id: string,
 	opts: UseQueryOptions<PlaceDetails, Error> | void
-) => {
-	console.log(id)
-	return useQuery<PlaceDetails, Error>({
-		queryKey: ['places', id],
+) =>
+	useQuery<PlaceDetails, Error>({
+		queryKey: ['place', id],
 		queryFn: async () =>
 			fetch(
 				`https://api.foursquare.com/v3/places/${id}?fields=${detailFieldsUri}`,
@@ -90,4 +94,55 @@ export const usePlaceDetails = (
 				.then((data) => data as PlaceDetails),
 		...opts,
 	})
+
+export const usePhotos = (
+	query: {
+		id: string
+		sort?: 'POPULAR' | 'NEWEST'
+		classifications?: 'food' | 'indoor' | 'menu' | 'outdoor'
+	},
+	opts: UseQueryOptions<Photo[], Error> | void
+) => {
+	const {id} = query
+	const sort = query.sort ?? 'POPULAR'
+	const classifications = query.classifications ?? ''
+
+	return useQuery<Photo[], Error>({
+		queryKey: ['photos', id, sort, classifications],
+		queryFn: async () =>
+			fetch(
+				`https://api.foursquare.com/v3/places/${id}/photos?sort=${sort}&classifications=${classifications}`,
+				reqOpts
+			)
+				.then(async (response) => response.json())
+				.then((data) => data as Photo[]),
+		...opts,
+	})
 }
+
+const tipFields = [
+	'id',
+	'created_at',
+	'text',
+	'url',
+	'lang',
+	'agree_count',
+	'disagree_count',
+]
+const tipFieldsUri = encodeURIComponent(tipFields.join(','))
+
+export const useTips = (
+	id: string,
+	opts: UseQueryOptions<Tip[], Error> | void
+) =>
+	useQuery<Tip[], Error>({
+		queryKey: ['tips', id],
+		queryFn: async () =>
+			fetch(
+				`https://api.foursquare.com/v3/places/${id}/tips?fields=${tipFieldsUri}`,
+				reqOpts
+			)
+				.then(async (response) => response.json())
+				.then((data) => data as Tip[]),
+		...opts,
+	})
