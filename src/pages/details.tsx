b484dd0ce55e70873c7c468bc7d82d22b62a33ac
riolly <alberto.riolly@gmail.com>
2022-12-05 13:11:34 +0700

    :construction: wip: usePhotos

--- src/pages/details.tsx
+++ src/pages/details.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react'
 import NavbarLayout from 'layouts/navbar'
 import {useMatch} from '@tanstack/react-router'
-import {usePlaceDetails} from 'utils/hooks'
-import {type PlaceDetails} from 'types/place'
-import place from 'assets/details-sample'
+import {usePlaceDetails, usePhotos} from 'utils/hooks'
+import {
+	type SortTypes,
+	type PlaceDetails,
+	type ClassificationTypes,
+	photosSort,
+	photosClassifications,
+} from 'types/place'
 
 const DetailsPage = () => {
 	const {params} = useMatch('/place/$id')
@@ -27,50 +32,76 @@ const DetailsPage = () => {
 	)
 }
 
-const sortTypes = [
-	'popular',
-	'newest',
-	'food',
-	'indoor',
-	'menu',
-	'outdoor',
-] as const
-type SortTypes = typeof sortTypes[number]
-
-const DetailedCard = ({name, description, photos}: PlaceDetails) => {
-	const [photoSort, setPhotoSort] = React.useState<SortTypes>('popular')
+const DetailedCard = ({fsq_id}: PlaceDetails) => {
+	const [sort, setSort] = React.useState<SortTypes>('popular')
+	const [classifications, setClassifications] =
+		React.useState<ClassificationTypes>('all')
+	const {data, isLoading, isError, error} = usePhotos({
+		id: fsq_id,
+		sort,
+		classifications,
+	})
+	const isEmpty = !data
 
 	return (
 		<div className='h-full space-y-4 bg-dark-bg/20'>
 			{/* #PHOTOS */}
 			<div className='bg-red-200s'>
 				{/* SNAP */}
-				<div className='bg-blue-200s snap-x snap-mandatory overflow-x-scroll whitespace-nowrap '>
-					{photos.map((photo) => (
-						<div
-							key={photo.id}
-							className='bg-green-200s inline-block h-60 w-full snap-center align-top'
-						>
-							<img
-								src={photo.prefix + '800x480' + photo.suffix}
-								className='h-full w-full object-cover'
-							/>
-						</div>
-					))}
+				<div className='bg-blue-200s h-60 w-full snap-x snap-mandatory overflow-x-scroll whitespace-nowrap '>
+					{isLoading ? (
+						<LoadingCard />
+					) : isError ? (
+						<ErrorCard error={error} />
+					) : isEmpty ? (
+						<EmptyCard />
+					) : (
+						data.map((photo) => (
+							<div
+								key={`${sort}_${classifications}_${photo.id}`}
+								className='bg-green-200s inline-block h-60 w-full snap-center align-top'
+							>
+								<img
+									src={photo.prefix + '800x480' + photo.suffix}
+									className='h-full w-full object-cover'
+								/>
+							</div>
+						))
+					)}
 				</div>
 
 				{/* #SORT */}
-				<div className='bg-green-200s px-8'>
+				<div className='bg-green-200s mt-1 px-8'>
 					<div className='hide-scrollbar bg-purple-200s h-fit space-x-1 overflow-y-auto whitespace-nowrap'>
-						{sortTypes.map((type) => (
+						{photosSort.map((type) => (
 							<button
 								key={type}
 								onClick={() => {
-									setPhotoSort(type)
+									setSort(type)
+									setClassifications('all')
 								}}
-								className='rounded-full bg-primary-darker px-2 align-bottom font-body text-base font-semibold leading-5'
+								className={`rounded-full bg-secondary-darker px-2 font-body text-base font-semibold leading-5 ${
+									type === sort
+										? 'border-2 border-secondary-normal bg-opacity-75'
+										: ''
+								}`}
 							>
 								{type}
+							</button>
+						))}
+						{photosClassifications.map((classification) => (
+							<button
+								key={classification}
+								onClick={() => {
+									setClassifications(classification)
+								}}
+								className={`rounded-full bg-primary-darker px-2 font-body text-base font-semibold leading-5 ${
+									classification === classifications
+										? 'border-2 border-primary-normal bg-opacity-75'
+										: ''
+								}`}
+							>
+								{classification}
 							</button>
 						))}
 					</div>
