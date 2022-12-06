9fff15f1a3e0a024633a1ba34d36bce5c6cc83a7
riolly <alberto.riolly@gmail.com>
2022-12-06 15:34:20 +0700

    :recycle: refactor: section splitting

--- src/pages/details.tsx
+++ src/pages/details.tsx
@@ -68,14 +68,14 @@ const DetailsPage = () => {
 				) : isEmpty ? (
 					<EmptyCard />
 				) : (
-					<DetailedCard {...data} />
+					<DetailsContent {...data} />
 				)}
 			</main>
 		</NavbarLayout>
 	)
 }
 
-const DetailedCard = ({
+const DetailsContent = ({
 	fsq_id,
 	name,
 	categories,
@@ -84,17 +84,48 @@ const DetailedCard = ({
 	hours,
 	price,
 	features,
-}: PlaceDetails) => {
+}: PlaceDetails) => (
+	<div className='h-full min-h-screen space-y-4 bg-dark-bg/20 pb-16'>
+		<div className='bg-red-200s'>
+			<PhotosSection fsq_id={fsq_id} />
+		</div>
+
+		<div className='bg-red-200s px-8'>
+			<TitleSection name={name} categories={categories} />
+		</div>
+
+		<div className='space-y-1 px-8'>
+			<DescriptionStatsSection
+				description={description}
+				price={price}
+				hours={hours}
+				popularity={popularity}
+			/>
+		</div>
+
+		<div className='space-y-2 px-8 pt-2 '>
+			<FeaturesSection features={features} />
+		</div>
+	</div>
+)
+
+const PhotosSection = ({fsq_id}: {fsq_id: string}) => {
 	const [sort, setSort] = React.useState<SortTypes>('popular')
 	const [classifications, setClassifications] =
 		React.useState<ClassificationTypes>('food')
-	const {data, isLoading, isError, error} = usePhotos({
+	const {
+		data: photos,
+		isLoading,
+		isError,
+		error,
+	} = usePhotos({
 		id: fsq_id,
 		sort,
 		classifications,
 	})
-	const isEmpty = !data
+	const isEmpty = !photos
 
+	const photoContainerRef = React.useRef<HTMLDivElement>(null)
 	const [photoActiveId, setPhotoActiveId] = React.useState(0)
 
 	const handlePhotosSnap = (event: React.UIEvent<HTMLDivElement>) => {
@@ -115,75 +146,73 @@ const DetailedCard = ({
 				? photoActiveId === 0
 					? 'bg-opacity-40 text-slate-500'
 					: 'bg-opacity-70 text-white'
-				: photoActiveId === data.length - 1
+				: photoActiveId === photos.length - 1
 				? 'bg-opacity-40 text-slate-500'
 				: 'bg-opacity-70 text-white'
 
 		return `absolute top-1/2 -translate-y-1/2 h-6 w-6 rounded-full bg-dark-bg p-[1px] ${activeClassName}`
 	}
 
-	const photoContainerRef = React.useRef<HTMLDivElement>(null)
-
 	return (
-		<div className='h-full min-h-screen space-y-4 bg-dark-bg/20 pb-16'>
-			{/* #PHOTOS */}
-			<div className='bg-red-200s'>
-				{/* SNAP */}
-				<div
-					className='bg-blue-200s hide-scrollbar relative h-60 w-full snap-x snap-mandatory overflow-x-scroll whitespace-nowrap rounded-b-xl shadow-xl shadow-primary-darkest/75'
-					onScroll={handlePhotosSnap}
-					ref={photoContainerRef}
-				>
-					{isLoading ? (
-						<LoadingCard />
-					) : isError ? (
-						<ErrorCard error={error} />
-					) : isEmpty ? (
-						<EmptyCard />
-					) : (
-						data.map((photo) => (
-							<div
-								key={`${sort}_${classifications}_${photo.id}`}
-								className='bg-green-200s inline-block h-60 w-full snap-center align-top'
-							>
-								<img
-									src={photo.prefix + '800x480' + photo.suffix}
-									className='h-full w-full object-cover'
-								/>
-							</div>
-						))
-					)}
-				</div>
+		<>
+			<div
+				className='bg-blue-200s hide-scrollbar relative h-60 w-full snap-x snap-mandatory overflow-x-scroll whitespace-nowrap rounded-b-xl shadow-xl shadow-primary-darkest/75'
+				onScroll={handlePhotosSnap}
+				ref={photoContainerRef}
+			>
+				{isLoading ? (
+					<LoadingCard />
+				) : isError ? (
+					<ErrorCard error={error} />
+				) : isEmpty ? (
+					<EmptyCard />
+				) : (
+					photos.map((photo) => (
+						<div
+							key={`${sort}_${classifications}_${photo.id}`}
+							className='bg-green-200s inline-block h-60 w-full snap-center align-top'
+						>
+							<img
+								src={photo.prefix + '800x480' + photo.suffix}
+								className='h-full w-full object-cover'
+							/>
+						</div>
+					))
+				)}
+			</div>
 
-				{data && data.length > 1 && (
-					<>
-						<button
-							className='bg-red-200s absolute top-0 h-60 w-8 px-1'
-							onClick={() => {
-								const container = photoContainerRef.current
-								if (container) {
-									container.scrollLeft -= container.clientWidth
-								}
-							}}
-						>
-							<ChevronLeftIcon className={getChevronClassName('left')} />
-						</button>
-						<button
-							className='bg-red-200s absolute top-0 right-0 h-60 w-8 px-1'
-							onClick={() => {
-								const container = photoContainerRef.current
-								if (container) {
-									container.scrollLeft += container.clientWidth
-								}
-							}}
-						>
-							<ChevronRightIcon className={getChevronClassName('right')} />
-						</button>
-						<div className='bg-red-200s absolute top-[13.5rem] flex h-6 w-full items-center justify-center gap-1 py-1'>
-							{data.map((photo, i) => (
-								<div
-									key={photo.id}
-									className={`
+			{/* #SCROLL NAVIGATION */}
+			{photos && photos.length > 1 && (
+				<>
+					<button
+						className='bg-red-200s absolute top-0 h-60 w-8 px-1'
+						onClick={() => {
+							const container = photoContainerRef.current
+							if (container) {
+								container.scrollLeft -= container.clientWidth
+							}
+						}}
+					>
+						<ChevronLeftIcon className={getChevronClassName('left')} />
+					</button>
+
+					<button
+						className='bg-red-200s absolute top-0 right-0 h-60 w-8 px-1'
+						onClick={() => {
+							const container = photoContainerRef.current
+							if (container) {
+								container.scrollLeft += container.clientWidth
+							}
+						}}
+					>
+						<ChevronRightIcon className={getChevronClassName('right')} />
+					</button>
+
+					<div className='bg-red-200s absolute top-[13.5rem] flex h-6 w-full items-center justify-center gap-1 py-1'>
+						{photos.map((photo, i) => (
+							<div
+								key={photo.id}
+								className={`
 											relative inline-block rounded-full bg-dark-bg
 											${
 												i === photoActiveId
@@ -191,120 +220,123 @@ const DetailedCard = ({
 													: 'h-2.5 w-2.5 bg-opacity-50'
 											}
 											`}
-								>
-									{i === photoActiveId && (
-										<div className='centered h-2 w-2 rounded-full bg-light-bg/75' />
-									)}
-								</div>
-							))}
-						</div>
-					</>
-				)}
-
-				{/* #SORT */}
-				<div className='bg-green-200s mt-1.5 px-8'>
-					<div className='hide-scrollbar bg-purple-200s h-fit space-x-1 overflow-y-auto whitespace-nowrap'>
-						{photosSort.map((type) => (
-							<button
-								key={type}
-								onClick={() => {
-									setSort(type)
-									setClassifications('all')
-								}}
-								className={`rounded-full bg-secondary-darker px-2 font-body text-base font-semibold leading-5 ${
-									type === sort
-										? 'border-2 border-secondary-normal bg-opacity-75'
-										: ''
-								}`}
 							>
-								{type}
-							</button>
-						))}
-						{photosClassifications.map((classification) => (
-							<button
-								key={classification}
-								onClick={() => {
-									setClassifications(classification)
-								}}
-								className={`rounded-full bg-primary-darker px-2 font-body text-base font-semibold leading-5 ${
-									classification === classifications
-										? 'border-2 border-primary-normal bg-opacity-75'
-										: ''
-								}`}
-							>
-								{classification}
-							</button>
-						))}
-					</div>
-				</div>
-			</div>
-
-			{/* #TITLE SECTION */}
-			<div className='bg-red-200s px-8'>
-				<h1 className='text-2xl'>{name}</h1>
-
-				<div className='flex items-center gap-1'>
-					<CheckBadgeIcon className='h-5 w-5 text-secondary-lighter' />
-					<span>|</span>
-					<div className='hide-scrollbar flex-1 space-x-1 overflow-auto whitespace-nowrap'>
-						{categories.map(({id, name}, i) => (
-							<span
-								key={id}
-								className='align-middle text-sm font-semibold text-opacity-75'
-							>
-								{name}
-								{i !== categories.length - 1 && (
-									<span className='ml-1 align-middle text-xs text-opacity-75'>
-										&bull;
-									</span>
+								{i === photoActiveId && (
+									<div className='centered h-2 w-2 rounded-full bg-light-bg/75' />
 								)}
-							</span>
+							</div>
 						))}
 					</div>
-				</div>
-			</div>
+				</>
+			)}
 
-			{/* #DESCRIPTION SECTION */}
-			<div className='space-y-1 px-8'>
-				<p className='leading-6 line-clamp-3'>{description}</p>
-				<div className='flex gap-4'>
-					{popularity >= 0.75 && (
-						<div className='flex items-center gap-1 rounded-full'>
-							<ArrowTrendingUpIcon className='inline h-6 w-6 text-secondary-lightest' />
-							<p className='text-base font-semibold'>Trending</p>
-						</div>
-					)}
-					<div className='flex items-center gap-1 rounded-full'>
-						<ClockIcon
-							className={`h-5 w-5 ${
-								hours.open_now ? 'text-blue-600' : 'text-red-500'
+			{/* #SORT PILLS */}
+			<div className='bg-green-200s mt-1.5 px-8'>
+				<div className='hide-scrollbar bg-purple-200s h-fit space-x-1 overflow-y-auto whitespace-nowrap'>
+					{photosSort.map((type) => (
+						<button
+							key={type}
+							onClick={() => {
+								setSort(type)
+								setClassifications('all')
+							}}
+							className={`rounded-full bg-secondary-darker px-2 font-body text-base font-semibold leading-5 ${
+								type === sort
+									? 'border-2 border-secondary-normal bg-opacity-75'
+									: ''
 							}`}
-						/>
-						<p className='text-base font-semibold'>
-							{hours.open_now ? 'Open' : 'Close'}
-						</p>
-						<ChevronDownIcon className='h-4 w-4 text-light-body' />
-					</div>
-					<div className='flex items-center gap-1 rounded-full'>
-						<MapPinIcon className='h-5 w-5 text-red-600' />
-						<p className='text-base font-semibold'>Maps</p>
-					</div>
-					<div className='flex items-center gap-1 rounded-full'>
-						<BanknotesIcon className='h-5 w-5 text-green-600' />
-						<p className='text-base font-semibold'>
-							{Array.from({length: price ?? 0}, () => '$').join('')}
-						</p>
-					</div>
+						>
+							{type}
+						</button>
+					))}
+					{photosClassifications.map((classification) => (
+						<button
+							key={classification}
+							onClick={() => {
+								setClassifications(classification)
+							}}
+							className={`rounded-full bg-primary-darker px-2 font-body text-base font-semibold leading-5 ${
+								classification === classifications
+									? 'border-2 border-primary-normal bg-opacity-75'
+									: ''
+							}`}
+						>
+							{classification}
+						</button>
+					))}
 				</div>
 			</div>
-
-			{/* #FEATURES SECTION */}
-			<div className='space-y-2 px-8 pt-2 '>
-				<Features features={features} />
-			</div>
-		</div>
+		</>
 	)
 }
+
+const TitleSection = ({
+	name,
+	categories,
+}: Pick<PlaceDetails, 'name' | 'categories'>) => (
+	<>
+		<h1 className='text-2xl'>{name}</h1>
+		<div className='flex items-center gap-1'>
+			<CheckBadgeIcon className='h-5 w-5 text-secondary-lighter' />
+			<span>|</span>
+			<div className='hide-scrollbar flex-1 space-x-1 overflow-auto whitespace-nowrap'>
+				{categories.map(({id, name}, i) => (
+					<span
+						key={id}
+						className='align-middle text-sm font-semibold text-opacity-75'
+					>
+						{name}
+						{i !== categories.length - 1 && (
+							<span className='ml-1 align-middle text-xs text-opacity-75'>
+								&bull;
+							</span>
+						)}
+					</span>
+				))}
+			</div>
+		</div>
+	</>
+)
+
+const DescriptionStatsSection = ({
+	description,
+	price,
+	hours,
+	popularity,
+}: Pick<PlaceDetails, 'description' | 'price' | 'hours' | 'popularity'>) => (
+	<>
+		<p className='leading-6 line-clamp-3'>{description}</p>
+		<div className='flex gap-4'>
+			{popularity >= 0.75 && (
+				<div className='flex items-center gap-1 rounded-full'>
+					<ArrowTrendingUpIcon className='inline h-6 w-6 text-secondary-lightest' />
+					<p className='text-base font-semibold'>Trending</p>
+				</div>
+			)}
+			<div className='flex items-center gap-1 rounded-full'>
+				<ClockIcon
+					className={`h-5 w-5 ${
+						hours.open_now ? 'text-blue-600' : 'text-red-500'
+					}`}
+				/>
+				<p className='text-base font-semibold'>
+					{hours.open_now ? 'Open' : 'Close'}
+				</p>
+				<ChevronDownIcon className='h-4 w-4 text-light-body' />
+			</div>
+			<div className='flex items-center gap-1 rounded-full'>
+				<MapPinIcon className='h-5 w-5 text-red-600' />
+				<p className='text-base font-semibold'>Maps</p>
+			</div>
+			<div className='flex items-center gap-1 rounded-full'>
+				<BanknotesIcon className='h-5 w-5 text-green-600' />
+				<p className='text-base font-semibold'>
+					{Array.from({length: price ?? 0}, () => '$').join('')}
+				</p>
+			</div>
+		</div>
+	</>
+)
 
 const Feature = ({Icon, label}: {label: string; Icon: IconType}) => (
 	<div className='col-span-1 flex flex-col items-center gap-0.5'>
@@ -316,7 +348,7 @@ const Feature = ({Icon, label}: {label: string; Icon: IconType}) => (
 )
 
 // eslint-disable-next-line complexity
-const Features = ({features}: {features: PlaceDetails['features']}) => {
+const FeaturesSection = ({features}: {features: PlaceDetails['features']}) => {
 	const {
 		payment: {credit_cards: creditCards, digital_wallet: digitalWallet},
 		food_and_drink: {meals, alcohol},
