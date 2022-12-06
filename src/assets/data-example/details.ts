d1ef550eabfeace1693d16204c6ae8b69d9e8c6a
riolly <alberto.riolly@gmail.com>
2022-12-07 00:05:18 +0700

    :bento: chore: details data example

---
+++ src/assets/data-example/details.ts
@@ -0,0 +1,316 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+export const place = {
+	fsq_id: '4aeb8764f964a52012c321e3',
+	categories: [
+		{
+			id: 13031,
+			name: 'Burger Joint',
+			icon: {
+				prefix: 'https://ss3.4sqi.net/img/categories_v2/food/burger_',
+				suffix: '.png',
+			},
+		},
+		{
+			id: 13145,
+			name: 'Fast Food Restaurant',
+			icon: {
+				prefix: 'https://ss3.4sqi.net/img/categories_v2/food/fastfood_',
+				suffix: '.png',
+			},
+		},
+	],
+	description:
+		"We are monitoring all local, state and federal regulations to provide customers with contactless ordering options. While some restaurants may have dining rooms open, others are serving customers through the Drive Thru, with Mobile Order & Pay on the McDonald's App, and with McDelivery ...",
+	email: 'ahargrave@phillipsaerospace.com',
+	features: {
+		payment: {
+			credit_cards: {
+				accepts_credit_cards: true,
+				discover: true,
+				visa: true,
+				diners_club: true,
+				master_card: true,
+			},
+			digital_wallet: {
+				accepts_nfc: true,
+			},
+		},
+		food_and_drink: {
+			alcohol: {
+				beer: true,
+				byo: true,
+				cocktails: true,
+				full_bar: true,
+				wine: true,
+			},
+			meals: {
+				breakfast: true,
+				brunch: true,
+				lunch: true,
+				happy_hour: true,
+				dessert: true,
+				dinner: true,
+			},
+		},
+		services: {
+			delivery: true,
+			takeout: true,
+			drive_through: true,
+			dine_in: {
+				reservations: true,
+				online_reservations: true,
+				groups_only_reservations: true,
+			},
+		},
+		amenities: {
+			restroom: true,
+			smoking: true,
+			jukebox: true,
+			music: true,
+			live_music: true,
+			private_room: true,
+			outdoor_seating: true,
+			tvs: true,
+			atm: true,
+			coat_check: true,
+			wheelchair_accessible: true,
+			parking: {
+				parking: true,
+				street_parking: true,
+				valet_parking: true,
+				public_lot: true,
+				private_lot: true,
+			},
+		},
+	},
+	geocodes: {
+		main: {
+			latitude: 32.718654,
+			longitude: -117.154087,
+		},
+		roof: {
+			latitude: 32.718654,
+			longitude: -117.154087,
+		},
+	},
+	hours: {
+		display: 'Open Daily 12:00 AM-11:59 PM',
+		is_local_holiday: false,
+		open_now: true,
+		regular: [
+			{
+				close: '2359',
+				day: 1,
+				open: '0000',
+			},
+			{
+				close: '2359',
+				day: 2,
+				open: '0000',
+			},
+			{
+				close: '2359',
+				day: 3,
+				open: '0000',
+			},
+			{
+				close: '2359',
+				day: 4,
+				open: '0000',
+			},
+			{
+				close: '2359',
+				day: 5,
+				open: '0000',
+			},
+			{
+				close: '2359',
+				day: 6,
+				open: '0000',
+			},
+			{
+				close: '2359',
+				day: 7,
+				open: '0000',
+			},
+		],
+		seasonal: [],
+	},
+	hours_popular: [
+		{
+			close: '0800',
+			day: 1,
+			open: '0600',
+		},
+		{
+			close: '1900',
+			day: 1,
+			open: '1800',
+		},
+		{
+			close: '0800',
+			day: 2,
+			open: '0600',
+		},
+		{
+			close: '1700',
+			day: 2,
+			open: '1500',
+		},
+		{
+			close: '0800',
+			day: 3,
+			open: '0600',
+		},
+		{
+			close: '0800',
+			day: 4,
+			open: '0600',
+		},
+		{
+			close: '1400',
+			day: 4,
+			open: '1100',
+		},
+		{
+			close: '0800',
+			day: 5,
+			open: '0600',
+		},
+		{
+			close: '2000',
+			day: 5,
+			open: '1900',
+		},
+		{
+			close: '1000',
+			day: 6,
+			open: '0900',
+		},
+		{
+			close: '1500',
+			day: 6,
+			open: '1400',
+		},
+		{
+			close: '2100',
+			day: 6,
+			open: '2000',
+		},
+		{
+			close: '0900',
+			day: 7,
+			open: '0800',
+		},
+	],
+	location: {
+		address: '1280 Park Blvd',
+		census_block: '060730052023002',
+		country: 'US',
+		cross_street: 'A St',
+		dma: 'San Diego',
+		formatted_address: '1280 Park Blvd (A St), San Diego, CA 92101',
+		locality: 'San Diego',
+		neighborhood: ['Central San Diego'],
+		po_box: 'P.O. Box 33929',
+		postcode: '92101',
+		region: 'CA',
+	},
+	menu: 'https://www.mcdonalds.com/us/en-us/full-menu.html',
+	name: "McDonald's",
+	photos: [
+		{
+			id: '5063c583e4b02f3d6d87c7e7',
+			created_at: '2012-09-27T03:18:27.000Z',
+			prefix: 'https://fastly.4sqi.net/img/general/',
+			suffix: '/32485244_Os4_teFveeLn1PgEIW2fx-P1DYflTAdCBvrTbhd5apg.jpg',
+			width: 480,
+			height: 640,
+		},
+		{
+			id: '4ffdd745e4b06b792d990fa6',
+			created_at: '2012-07-11T19:43:01.000Z',
+			prefix: 'https://fastly.4sqi.net/img/general/',
+			suffix: '/b-r2dttMDYXeZ_wDLU6lVp2aewkeAdoMuiwymuQFQ8g.jpg',
+			width: 720,
+			height: 538,
+			classifications: ['outdoor'],
+		},
+		{
+			id: '508c7ea6e4b086e59a3996a9',
+			created_at: '2012-10-28T00:39:02.000Z',
+			prefix: 'https://fastly.4sqi.net/img/general/',
+			suffix: '/36304139_YOy431c8cFtxvP1cyZEnU5AcN12xluvAbi_aBqTKylg.jpg',
+			width: 540,
+			height: 720,
+			classifications: ['outdoor'],
+		},
+		{
+			id: '4e6a64f462e1cd1d54ae4afa',
+			created_at: '2011-09-09T19:11:48.000Z',
+			prefix: 'https://fastly.4sqi.net/img/general/',
+			suffix: '/Y5H4HE4KIQL01ACWHZMK2SPGFKJVRPBZ2ATYAQ41C1EIBKZT.jpg',
+			width: 537,
+			height: 720,
+		},
+		{
+			id: '62efdb79db9f082c52cf5c47',
+			created_at: '2022-08-07T15:34:17.000Z',
+			prefix: 'https://fastly.4sqi.net/img/general/',
+			suffix: '/1993682_jEW41asIJ1LJZDz6ylNxv6DyK_CPxwgppsuNSGUJLck.jpg',
+			width: 1440,
+			height: 1920,
+		},
+	],
+	popularity: 0.9992352958747797,
+	price: 1,
+	rating: 7.6,
+	related_places: {},
+	social_media: {
+		facebook_id: '111698698865526',
+		instagram: 'mcdonalds',
+		twitter: 'mcdonalds',
+	},
+	stats: {
+		total_photos: 45,
+		total_ratings: 244,
+		total_tips: 15,
+	},
+	tastes: [
+		'breakfast food',
+		'lunch',
+		'eggs',
+		'dinner',
+		'muffins',
+		'good for a quick meal',
+		'hash browns',
+		'chocolate cookies',
+		'classical music',
+	],
+	tel: '(619) 550-1823',
+	tips: [
+		{
+			created_at: '2017-08-22T04:37:01.000Z',
+			text: 'Plenty of cops in the Downtown area. Which makes this one of the more entertaining McDonald’s of America.',
+		},
+		{
+			created_at: '2017-07-23T06:25:53.000Z',
+			text: 'Slow drive through',
+		},
+		{
+			created_at: '2016-11-28T17:09:34.000Z',
+			text: 'Introducing sweet, savory chicken for breakfast, available all day. Served on your choice of a flaky biscuit, sweet McGriddles Cakes or toasted, buttery McMuffin. YAAAS. Enough said.',
+		},
+		{
+			created_at: '2016-11-28T17:09:34.000Z',
+			text: 'Introducing sweet, savory chicken for breakfast, available all day. Served on your choice of a flaky biscuit, sweet McGriddles Cakes or toasted, buttery McMuffin. YAAAS. Enough said.',
+		},
+		{
+			created_at: '2016-11-22T15:08:37.000Z',
+			text: 'Try the New Sriracha Mac Sauce™ on a Big Mac® or on the side to dip your McNuggets® or Fries. It’s given a kick to the classic you love.',
+		},
+	],
+	verified: true,
+	website: 'https://www.mcdonalds.com/',
+}
+export default place
